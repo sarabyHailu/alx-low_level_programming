ba3ec1305f17479290ba4aada7ff51cac900dacd
sarabyHailu <marsilashailu@gmail.com>
2022-10-10 09:05:10 -0700

    task 0

--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * File: main
- *
- * Desc: header file
- */
-int _putchar(char c);
+
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
 
 #endif
